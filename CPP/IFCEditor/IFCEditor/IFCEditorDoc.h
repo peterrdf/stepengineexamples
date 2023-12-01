5443960c1f1061056dd179be8d894ebc201646e3
Svilen M. Varbanov <svilen_varbanov@msn.com>
2023-12-01 09:00:18 +0200

    IFCEditor

---
+++ CPP/IFCEditor/IFCEditor/IFCEditorDoc.h
@@ -0,0 +1,48 @@
+
+// IFCEditorDoc.h : interface of the CIFCEditorDoc class
+//
+
+
+#pragma once
+
+
+class CIFCEditorDoc : public CDocument
+{
+protected: // create from serialization only
+	CIFCEditorDoc() noexcept;
+	DECLARE_DYNCREATE(CIFCEditorDoc)
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// Implementation
+public:
+	virtual ~CIFCEditorDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Generated message map functions
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// Helper function that sets search content for a Search Handler
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
