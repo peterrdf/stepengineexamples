5214a259170fe513a66676a6211bc10ee8e9da04
Svilen M. Varbanov <svilen_varbanov@msn.com>
2024-09-09 15:45:53 +0300

    Build.

--- bin/32bit/WASM/emifcengine.js
+++ bin/32bit/WASM/emifcengine.js
@@ -7234,8 +7234,8 @@ var dynCall_viijii = Module['dynCall_viijii'] = createExportWrapper('dynCall_vii
 var dynCall_iiiiij = Module['dynCall_iiiiij'] = createExportWrapper('dynCall_iiiiij');
 var dynCall_iiiiijj = Module['dynCall_iiiiijj'] = createExportWrapper('dynCall_iiiiijj');
 var dynCall_iiiiiijj = Module['dynCall_iiiiiijj'] = createExportWrapper('dynCall_iiiiiijj');
-var ___start_em_js = Module['___start_em_js'] = 10200376;
-var ___stop_em_js = Module['___stop_em_js'] = 10200648;
+var ___start_em_js = Module['___start_em_js'] = 10200488;
+var ___stop_em_js = Module['___stop_em_js'] = 10200760;
 
 // include: postamble.js
 // === Auto-generated postamble setup entry stuff ===
