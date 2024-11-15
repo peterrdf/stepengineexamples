c1305381195a2f3a7508f6d5b748e0c96f8de0d3
Svilen M. Varbanov <svilen_varbanov@msn.com>
2024-11-15 16:44:20 +0200

    Refactoring.

--- EmbeddedGeometryKernel/C++/StreamingInOut/StreamingInOut/StreamingInOut.cpp
+++ EmbeddedGeometryKernel/C++/StreamingInOut/StreamingInOut/StreamingInOut.cpp
@@ -109,13 +109,15 @@ BOOL CStreamingInOutApp::InitApplication()
 
 	ASSERT(importFileName == nullptr && exportFileName == nullptr && i > 0);
 
-	importFileName = new wchar_t[i + wcslen(L"exampleFile.bin") + 1];
+	const wchar_t* szInput = L"data\\EGK_StreamingInOut_exampleFile";
+	importFileName = new wchar_t[i + wcslen(szInput) + 1];
 	memcpy(&importFileName[0], this->m_pszHelpFilePath, i * sizeof(wchar_t));
-	memcpy(&importFileName[i], L"exampleFile.bin", (wcslen(L"exampleFile.bin") + 1) * sizeof(wchar_t));
+	memcpy(&importFileName[i], szInput, (wcslen(szInput) + 1) * sizeof(wchar_t));
 
-	exportFileName = new wchar_t[i + wcslen(L"exportedFile.bin") + 1];
+	const wchar_t* szOutput = L"EGK_StreamingInOut_exportedFile";
+	exportFileName = new wchar_t[i + wcslen(szOutput) + 1];
 	memcpy(&exportFileName[0], this->m_pszHelpFilePath, i * sizeof(wchar_t));
-	memcpy(&exportFileName[i], L"exportedFile.bin", (wcslen(L"exportedFile.bin") + 1) * sizeof(wchar_t));
+	memcpy(&exportFileName[i], szOutput, (wcslen(szOutput) + 1) * sizeof(wchar_t));
 
 	return CWinApp::InitApplication();
 }
