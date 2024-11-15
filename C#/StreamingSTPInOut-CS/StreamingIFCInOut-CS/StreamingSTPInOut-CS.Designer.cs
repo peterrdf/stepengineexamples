e9f3335b1bf21245e0c0051bca6066eb71175496
Svilen M. Varbanov <svilen_varbanov@msn.com>
2024-11-15 14:30:00 +0200

    Refactoring.

--- C#/StreamingSTPInOut-CS/StreamingIFCInOut-CS/StreamingSTPInOut-CS.Designer.cs
+++ C#/StreamingSTPInOut-CS/StreamingIFCInOut-CS/StreamingSTPInOut-CS.Designer.cs
@@ -35,7 +35,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(8, 8);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(191, 81);
             this.button1.TabIndex = 0;
@@ -50,9 +50,11 @@
             this.ClientSize = new System.Drawing.Size(207, 97);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "StreamingSTPInOut-CS";
             this.ResumeLayout(false);
 
         }
