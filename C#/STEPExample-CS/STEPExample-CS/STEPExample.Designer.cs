75b316e806f419e75d9c0ac27fffc6900ba321b8
Svilen M. Varbanov <svilen_varbanov@msn.com>
2024-11-16 07:19:15 +0200

    Refactoring.

--- C#/STEPExample-CS/STEPExample-CS/STEPExample.Designer.cs
+++ C#/STEPExample-CS/STEPExample-CS/STEPExample.Designer.cs
@@ -94,7 +94,7 @@
             this.buttonFindAssemblies.UseVisualStyleBackColor = true;
             this.buttonFindAssemblies.Click += new System.EventHandler(this.buttonFindAssemblies_Click);
             // 
-            // Form1
+            // STEPExample
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -108,7 +108,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "Form1";
+            this.Name = "STEPExample";
             this.Text = "STEPExample";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
